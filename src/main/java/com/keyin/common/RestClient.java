755c9f49451ebb769c447334afc6dbceaee5f9c2
Jamie Cornick <jamie.cornick@keyin.com>
2026-02-10 10:02:20 -0330

    Added calling a REST API

--- src/main/java/com/keyin/common/RestClient.java
+++ src/main/java/com/keyin/common/RestClient.java
@@ -2,12 +2,34 @@ package com.keyin.common;
 
 import com.keyin.aircraft.Aircraft;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class RestClient {
     private String URLRoot;
+    private HttpClient client;
 
     public String sendGetRequest(Aircraft aircraft) {
-        //TODO: Handle construction of the URL properly
-        return "{ \"status\": \"no-status\" }";
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(URLRoot)).build();
+
+        try {
+            HttpResponse<String> response = getClient().send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == 200) {
+                System.out.println("***** " + response.body());
+            } else {
+                System.out.println("Error Status Code: " + response.statusCode());
+            }
+
+            return response.body();
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "";
     }
 
     public String getURLRoot() {
@@ -16,5 +38,13 @@ public class RestClient {
 
     public void setURLRoot(String URLRoot) {
         this.URLRoot = URLRoot;
+    }
+
+    public HttpClient getClient() {
+        if (client == null) {
+            client  = HttpClient.newHttpClient();
+        }
+
+        return client;
     }
 }
